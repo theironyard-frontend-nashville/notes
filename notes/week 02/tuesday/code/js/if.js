d9541bcbcc00ef3fc1c64f188462ac087cae6380
William Golden <egdelwonk@gmail.com>
2015-10-13 16:47:13 -0500

    added code for tuesday

---
+++ notes/week 02/tuesday/code/js/if.js
@@ -0,0 +1,12 @@
+var name1 = 'kate';
+var name2 = "george";
+
+if (name1 == 'kate') {
+  console.log('kate is soooooo awesome!!!!!!');
+}
+
+if (name2 == 'will'){
+  console.log('will is lame, ugh');
+} else {
+  console.log('you are ok... i guess');
+}
